4799717e63123bee63805350747efd8ae64bc27c
Arun Dhaduti <arundhaduti@gmail.com>
2026-01-10 23:55:52 +0530

    Restructure: move my code to root

--- Week 1/Vibha/frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API = axios.create({
-  baseURL: "http://localhost:8000",
-});
-
-export const sendMessage = (message) => API.post("/chat", { message });
-
-export const resetChat = () => API.post("/reset");
